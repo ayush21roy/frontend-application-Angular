3c0607e6876fcb22e1daccd7149f97c1a0d4374f
ayush21roy <4nn20cs012@nieit.ac.in>
2026-01-20 12:06:04 +0530

    constants

---
+++ src/app/core/constants/api.constant.ts
@@ -0,0 +1,7 @@
+export const AUTH_API_CONST = {
+  LOGIN: 'https://dummyjson.com/auth/login',
+};
+
+export const DASHBOARD_API_CONST = {
+  GET_USERS: 'https://dummyjson.com/users',
+};
---
+++ src/app/core/constants/message.constant.ts
@@ -0,0 +1,4 @@
+export const MESSAGE_CONST = {
+  LOGIN_FAILED: 'Login failed. Please try again.',
+  FAILED_TO_LOAD_USERS: 'Failed to load users. Please try again later.',
+};
