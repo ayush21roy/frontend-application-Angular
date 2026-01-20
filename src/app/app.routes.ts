15f597d0c57d239da85df34c99e98f7021035b53
ayush21roy <4nn20cs012@nieit.ac.in>
2026-01-20 12:05:53 +0530

    app-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { authGuard, publicGuard } from './core/guards/auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    canActivate: [publicGuard],
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'home',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
